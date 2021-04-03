e8e88eec8d015ad28532612e7f1fb8ecf8c9dc40
sochoa <sean.m.ochoa@gmail.com>
2021-04-02 22:35:07 -0700

    now with routes in a definitions file

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,35 +1,13 @@
 import Header from './components/Header.js'
 import RequestRouter from './components/RequestRouter.js'
 
-import Home from './components/Home.js'
-import NewAccount from './components/NewAccount.js'
-import SignIn from './components/SignIn.js'
+import {AVAILABLE_ROUTES} from './definitions/routes.js'
 
 function App() {
-  const availableRoutes = [
-    {
-      component: Home,
-      key: "home",
-      path: "/",
-      label: "Home"
-    },
-    {
-      component: NewAccount,
-      key: "new-account",
-      path: "/new-account",
-      label: "Create an Account"
-    },
-    {
-      component: SignIn,
-      key: "sign-in",
-      path: "/sign-in",
-      label: "Sign In"
-    },
-  ]
   return (
     <div className='container'>
-      <RequestRouter routes={availableRoutes} />
-      <Header routes={availableRoutes} />
+      <RequestRouter routes={AVAILABLE_ROUTES} />
+      <Header routes={AVAILABLE_ROUTES} />
     </div>
   );
 }
